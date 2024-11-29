1b52efa5a611b5bbe6829b7680fdf9bf8b62ef66
Oleksii Popov <alexejpopovua@gmail.com>
2024-11-29 23:27:44 +0100

    refactor: cleanup the ecs setup

--- cfg/configuration.ts
+++ cfg/configuration.ts
@@ -1,27 +1,26 @@
 export default {
-    COMMON: {
-        project: "web-perf-mon",
-        region: process.env?.AWS_DEPLOYMENT_REGION || "",
-        account: process.env?.AWS_ACCOUNT || "",
-        defaultEnvironment: process.env?.DEFAULT_BRANCH || "main"
-    },
-    HOSTING: {
-        hostedZoneID: process.env?.HOSTED_ZONE_ID || "",
-        hostedZoneName: "oleksiipopov.com",
-        staticDomainName: "oleksiipopov.com",
-        apiDomainName: "task.perf-mon.examples.oleksiipopov.com"
-    },
-    REPORTING: {
-        bucketName: "web-perf-mon-reports",
-        temporaryBucketName: "web-perf-mon-reports-temp",
-        reportsDomainName: "reports.perf-mon.examples.oleksiipopov.com"
-    },
-    ANALYSIS: {
-        clusterName: 'web-perf-mon-cluster',
-        taskFamily: 'web-perf-mon-task-family-sitespeedio'
-    },
-    NETWORKING: {
-        vpcId: "vpc-fbcab982",
-        availabilityZones: ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"],
-    }
+  COMMON: {
+    project: "web-perf-mon",
+    region: process.env?.AWS_DEPLOYMENT_REGION || "",
+    account: process.env?.AWS_ACCOUNT || "",
+    defaultEnvironment: process.env?.DEFAULT_BRANCH || "main"
+  },
+  HOSTING: {
+    hostedZoneID: process.env?.HOSTED_ZONE_ID || "",
+    hostedZoneName: "oleksiipopov.com",
+    staticDomainName: "oleksiipopov.com",
+    apiDomainName: "task.perf-mon.examples.oleksiipopov.com"
+  },
+  REPORTING: {
+    bucketName: "web-perf-mon-reports",
+    temporaryBucketName: "web-perf-mon-reports-temp",
+    reportsDomainName: "reports.perf-mon.examples.oleksiipopov.com"
+  },
+  ANALYSIS: {
+    clusterName: 'web-perf-mon-cluster',
+    taskFamily: 'web-perf-mon-task-family-sitespeedio'
+  },
+  NETWORKING: {
+    securityGroupName: `web-perf-mon-ecs-sg`,
+  }
 };
--- lib/analysis-execution-stack.ts
+++ lib/analysis-execution-stack.ts
@@ -2,8 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import {Secret} from "aws-cdk-lib/aws-secretsmanager";
-import {PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {RetentionDays} from "aws-cdk-lib/aws-logs";
 
 import configuration from "../cfg/configuration";
@@ -11,13 +9,6 @@ import configuration from "../cfg/configuration";
 export class AnalysisExecutionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Create a VPC
-    const defaultVpc = ec2.Vpc.fromVpcAttributes(this, `${configuration.COMMON.project}-ecs-vpc-default`, {
-      availabilityZones: configuration.NETWORKING.availabilityZones,
-      vpcId: configuration.NETWORKING.vpcId,
-      region: props!.env!.region,
-    });
 
     const vpc = new ec2.Vpc(this, `${configuration.COMMON.project}-ecs-vpc`, {
       maxAzs: 2,
@@ -35,13 +26,8 @@ export class AnalysisExecutionStack extends cdk.Stack {
 
     // security group with all outbound traffic allowed
     new ec2.SecurityGroup(this, `${configuration.COMMON.project}-ecs-sg`, {
+      securityGroupName: configuration.NETWORKING.securityGroupName,
       vpc,
-      allowAllOutbound: true,
-    });
-
-    new ec2.SecurityGroup(this, `${configuration.COMMON.project}-ecs-sg-def`, {
-      securityGroupName: `${configuration.COMMON.project}-ecs-sg-def`,
-      vpc: defaultVpc,
       allowAllOutbound: true,
     });
 
@@ -56,37 +42,11 @@ export class AnalysisExecutionStack extends cdk.Stack {
       family: configuration.ANALYSIS.taskFamily,
       cpu: 4096,
       memoryLimitMiB: 8192,
-      executionRole: new Role(this, `${configuration.COMMON.project}-sitespeedio-task-execution-role`, {
-        roleName: `${configuration.COMMON.project}-sitespeedio-task-execution-role`,
-        assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
-        managedPolicies: [
-          {
-            managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy'
-          }
-        ],
-      }),
     });
-
-    // Create a Docker Hub secret
-    const dockerHubSecret = Secret.fromSecretNameV2(this, 'DockerHubSecret', 'docker-hub-secret');
-
-    //dockerHubSecret.grantRead(taskDefinition.taskRole);
-    taskDefinition.executionRole && taskDefinition.executionRole.addToPrincipalPolicy(PolicyStatement.fromJson({
-      "Effect": "Allow",
-      "Action": [
-        "secretsmanager:GetSecretValue"
-      ],
-      "Resource": [
-        dockerHubSecret.secretArn
-      ]
-    }));
-    taskDefinition.executionRole && dockerHubSecret.grantRead(taskDefinition.executionRole);
 
     // Add container to the task definition
     taskDefinition.addContainer(`${configuration.COMMON.project}-sitespeedio-container`, {
-      image: ecs.ContainerImage.fromRegistry('sitespeedio/sitespeed.io:35.6.1', {
-        //credentials: dockerHubSecret
-      }),
+      image: ecs.ContainerImage.fromRegistry('sitespeedio/sitespeed.io:35.6.1'),
       memoryLimitMiB: 8192,
       cpu: 4096,
       command: ['https://oleksiipopov.com', '--summary'],
