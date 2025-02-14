fa5b62c39287f735086a9d42bc865c38f23a6ff0
Oleksii Popov <alexejpopovua@gmail.com>
2025-02-14 20:51:57 +0100

    chore: remove old todos

--- lib/task-processing-constructs/StateMachineConstruct.ts
+++ lib/task-processing-constructs/StateMachineConstruct.ts
@@ -130,8 +130,6 @@ export class StateMachineConstruct extends Construct {
       }
     });
 
-    // TODO: Task to process each item in the map state. Initiates and waits a Fargate task to finish
-    // TODO: sitespeed.io uploads a single report to an S3 directory
     // TODO: sitespeed.io sends metrics to Grafana cloud
 
     const fargateTaskRunner = new tasks.EcsRunTask(this, `${configuration.COMMON.project}-run-fargate-task`, {
