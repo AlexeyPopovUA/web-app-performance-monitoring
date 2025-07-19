692eae9d866795f650642428b87f6aa171465792
Oleksii Popov <alexejpopovua@gmail.com>
2025-07-19 15:41:49 +0200

    refactor: fix TS errors

--- lib/lambda/steps/analysis-initiator-step/analysis-initiator-step.ts
+++ lib/lambda/steps/analysis-initiator-step/analysis-initiator-step.ts
@@ -10,41 +10,40 @@ export async function handler(initialState: AnalysisInitiatorInput): Promise<Ana
     ...initialState,
 
     command: [
-      ...(initialState.urls),
+      ...initialState.urls,
 
       // Browser
-      "--browser", initialState.browser,
-      "--browsertime.iterations", initialState.iterations.toString(),
-      "--browsertime.preWarmServer", "true",
-      "--browsertime.firefox.includeResponseBodies",
-      "--browsertime.chrome.includeResponseBodies",
-      "--browsertime.chrome.includeResponseBodies", "html", // "all"
-      "--browsertime.chrome.timeline", "true",
-      "--browsertime.chrome.collectConsoleLog", "true",
-      "--browsertime.chrome.collectLongTasks", "true",
-      "--browsertime.viewPort", "1920x1080",
-      "--groupAlias", initialState.variantName,
-      "--browsertime.visualMetrics", "true",
-      "--browsertime.visualMetricsPerceptual", "true",
-      "--browsertime.visualMetricsContentful", "true",
+      '--browser', initialState.browser,
+      '--browsertime.iterations', initialState.iterations.toString(),
+      '--browsertime.preWarmServer', 'true',
+      '--browsertime.firefox.includeResponseBodies', 'html',
+      '--browsertime.chrome.includeResponseBodies', 'html',
+      '--browsertime.chrome.timeline', 'true',
+      '--browsertime.chrome.collectConsoleLog', 'true',
+      '--browsertime.chrome.collectLongTasks', 'true',
+      '--browsertime.viewPort', '1920x1080',
+      '--groupAlias', initialState.variantName,
+      '--browsertime.visualMetrics', 'true',
+      '--browsertime.visualMetricsPerceptual', 'true',
+      '--browsertime.visualMetricsContentful', 'true',
 
       // S3 bucket configuration
-      ...(temporaryBucketName ? ["--s3.bucketname", temporaryBucketName] : []),
-      ...(temporaryBucketRegion ? ["--s3.region", temporaryBucketRegion] : []),
-      ...(initialState.reportPath ? ["--s3.path", initialState.reportPath] : []),
+      ...(temporaryBucketName ? ['--s3.bucketname', temporaryBucketName] : []),
+      ...(temporaryBucketRegion ? ['--s3.region', temporaryBucketRegion] : []),
+      ...(initialState.reportPath ? ['--s3.path', initialState.reportPath] : []),
 
       // Graphite configuration
-      "--graphite.host", domainNameRelay,
+      '--graphite.host', domainNameRelay as string,
 
       // Sustainable
-      "--sustainable.enable", "false",
+      '--sustainable.enable', 'false',
 
       // Options
-      "--graphite.addSlugToKey", "true",
-      "--copyLatestFilesToBase", "true",
+      '--graphite.addSlugToKey', 'true',
+      '--copyLatestFilesToBase', 'true',
       // TODO Add alias support
-      "--slug", "firstView",
-      "--plugins.add", "@sitespeed.io/plugin-lighthouse",
+      '--slug', 'firstView',
+      '--plugins.add', '@sitespeed.io/plugin-lighthouse',
       // Debug logging level
       //'-vvv',
       //"--resultBaseURL", "https://reports.perf-mon.examples.oleksiipopov.com",
